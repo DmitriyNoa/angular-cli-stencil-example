3c932f19c841dd05ee2eea8b19f93a81af6d0fc7
dzharkov <dmytro.zharkov@zalando.de>
2018-05-17 16:29:27 +0200

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { products } from '../mocks/products'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public products: any = products;
+  public likedProducts: any = [];
+
+  public onProductLiked(evt: any) {
+    this.likedProducts.push(evt.detail);
+  }
 }
---
+++ src/mocks/products.ts
@@ -0,0 +1,20 @@
+export const products: any = [
+  {
+    name: "Langarmshirt - black/legion green/reflective silver",
+    image: "https://mosaic01.ztat.net/vgs/media/catalog-sm/TP/72/1D/0D/FJ/11/TP721D0DF-J11@10.jpg",
+    brand: "Nike Performance",
+    price: {
+      old: "€19,99",
+      current: "€12,49"
+    }
+  },
+  {
+    name: "Langarmshirt - black/legion green/reflective orange",
+    image: "https://mosaic01.ztat.net/vgs/media/catalog-sm/TP/72/1D/0D/FJ/11/TP721D0DF-J11@10.jpg",
+    brand: "Nike Performance",
+    price: {
+      old: "€129,99",
+      current: "€122,49"
+    }
+  }
+]
